0dd802a6cae680901f973801cc28c1b43c10e8da
Jan Křůmal <xkruma01@vutbr.cz>
2025-03-10 13:57:44 +0100

    [RUN SCRIPT] Updated folders

--- JCG/run.sh
+++ JCG/run.sh
@@ -7,8 +7,8 @@ ALGORITHM="RTA"
 RUNS=1                
 
 # Paths
-JCG_PATH="/mnt/c/projects/BP/code/JCG"
-INPUT_DIR="dummy_input"
+JCG_PATH="/home/krumi/IdeaProjects/BP/JCG"
+INPUT_DIR="input/java"
 OUTPUT_DIR="testcasesOutput/java/native_image_adapter"
 
 mkdir -p "$OUTPUT_DIR"
@@ -21,7 +21,10 @@ mkdir -p "$OUTPUT_DIR"
 # run
 echo "Running analysis with NativeImageJCGAdapter..."
 
-sbt "; project jcg_evaluation; runMain Evaluation --input $JCG_PATH/$INPUT_DIR --output $JCG_PATH/$OUTPUT_DIR --adapter $TOOL --algorithm-prefix $ALGORITHM"
+sbt "; project jcg_evaluation; runMain Evaluation --input $JCG_PATH/$INPUT_DIR --output
+$JCG_PATH/$OUTPUT_DIR --adapter $TOOL --algorithm-prefix $ALGORITHM"
 
 
 echo "Analysis completed. Results are in $OUTPUT_DIR."
+
+# debug opt -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
