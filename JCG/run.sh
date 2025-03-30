0a3dfd3e9727f912c5df5211bc7514654a724bfe
Jan Křůmal <xkruma01@vutbr.cz>
2025-03-30 16:01:45 +0200

    [SCRIPT] Minor cleanup

--- JCG/run.sh
+++ JCG/run.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-# project name and tools
-PROJECT="test_project" 
+# Tested tool and algorithm
 TOOL="NativeImage"      
-ALGORITHM="RTA"         
-RUNS=1                
+ALGORITHM="PTA"
 
 # Paths
 JCG_PATH="/home/krumi/IdeaProjects/BP/JCG"
-INPUT_DIR="input/java"
+INPUT_DIR="input"
 OUTPUT_DIR="testcasesOutput/java/native_image_adapter"
+
+# Log files
+TEST_COMPILE_STD="test_compile_output.log"
+TESTS_COMPILE_ERR="test_compile_output_err.log"
+EVALUATION_STD="evaluation_fingerprint.log"
+EVALUATION_ERR="evaluation_fingerprint_err.log"
 
 mkdir -p "$OUTPUT_DIR"
 
-# compiling
-#echo "Building project with NativeImageJCGAdapter..."
-#sbt -java-home /opt/jdk8u342-b07/jre -J-Xmx400G "; project jcg_evaluation; compile"
-#sbt "; project jcg_evaluation; compile"
+# compile tests
+echo "Compiling JCG tests..."
+touch $TEST_COMPILE_STD
+touch $TESTS_COMPILE_ERR
+sbt -J-Xmx12G "project jcg_testcases" "runMain TestCaseExtractor --rsrcDir ./jcg_testcases/src/main/resources
+--outDir ./input --lang java --debug" > $TEST_COMPILE_STD 2> $TESTS_COMPILE_ERR
+echo "JCG tests compiled, compile output in files ${TEST_COMPILE_STD} and ${TESTS_COMPILE_ERR}."
 
-# run
+# run analysis
 echo "Running analysis with NativeImageJCGAdapter..."
+touch $EVALUATION_STD
+touch $EVALUATION_ERR
+sbt -J-Xmx12G "; project jcg_evaluation; runMain FingerprintExtractor -i $JCG_PATH/$INPUT_DIR/java
+ -o $JCG_PATH/$OUTPUT_DIR -l java --adapter $TOOL --algorithm-prefix $ALGORITHM" > $EVALUATION_STD 2> $EVALUATION_ERR
+echo "NativeImageJCGAdapter analysis completed, debug output in files ${EVALUATION_STD} and ${EVALUATION_ERR}."
 
-sbt "; project jcg_evaluation; runMain Evaluation --input $JCG_PATH/$INPUT_DIR --output
-$JCG_PATH/$OUTPUT_DIR --adapter $TOOL --algorithm-prefix $ALGORITHM"
-
-
-echo "Analysis completed. Results are in $OUTPUT_DIR."
-
-# debug opt -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
+# timings
+#sbt -J-Xmx12G "; project jcg_evaluation; runMain Evaluation --input $JCG_PATH/$INPUT_DIR --output $JCG_PATH/$OUTPUT_DIR --adapter $TOOL --algorithm-prefix $ALGORITHM"
