287b55da4ec5c29df2543f8e7c67212be4c50914
Jan Křůmal <xkruma01@vutbr.cz>
2025-03-31 16:47:01 +0200

    [SCRIPT] Log simplified

--- JCG/run.sh
+++ JCG/run.sh
@@ -10,28 +10,24 @@ INPUT_DIR="input"
 OUTPUT_DIR="testcasesOutput/java/native_image_adapter"
 
 # Log files
-TEST_COMPILE_STD="test_compile_output.log"
-TESTS_COMPILE_ERR="test_compile_output_err.log"
-EVALUATION_STD="evaluation_fingerprint.log"
-EVALUATION_ERR="evaluation_fingerprint_err.log"
+TEST_COMPILE="test_compile_output.log"
+EVALUATION="evaluation_fingerprint.log"
 
 mkdir -p "$OUTPUT_DIR"
 
 # compile tests
 echo "Compiling JCG tests..."
-touch $TEST_COMPILE_STD
-touch $TESTS_COMPILE_ERR
+touch $TEST_COMPILE
 sbt -J-Xmx12G "project jcg_testcases" "runMain TestCaseExtractor --rsrcDir ./jcg_testcases/src/main/resources
---outDir ./input --lang java --debug" > $TEST_COMPILE_STD 2> $TESTS_COMPILE_ERR
-echo "JCG tests compiled, compile output in files ${TEST_COMPILE_STD} and ${TESTS_COMPILE_ERR}."
+--outDir ./input --lang java --debug" &> $TEST_COMPILE
+echo "JCG tests compiled, compile output in file ${TEST_COMPILE}."
 
 # run analysis
 echo "Running analysis with NativeImageJCGAdapter..."
-touch $EVALUATION_STD
-touch $EVALUATION_ERR
+touch $EVALUATION
 sbt -J-Xmx12G "; project jcg_evaluation; runMain FingerprintExtractor -i $JCG_PATH/$INPUT_DIR/java
- -o $JCG_PATH/$OUTPUT_DIR -l java --adapter $TOOL --algorithm-prefix $ALGORITHM" > $EVALUATION_STD 2> $EVALUATION_ERR
-echo "NativeImageJCGAdapter analysis completed, debug output in files ${EVALUATION_STD} and ${EVALUATION_ERR}."
+ -o $JCG_PATH/$OUTPUT_DIR -l java --adapter $TOOL --algorithm-prefix $ALGORITHM" &> $EVALUATION
+echo "NativeImageJCGAdapter analysis completed, debug output in file ${EVALUATION}."
 
 # timings
 #sbt -J-Xmx12G "; project jcg_evaluation; runMain Evaluation --input $JCG_PATH/$INPUT_DIR --output $JCG_PATH/$OUTPUT_DIR --adapter $TOOL --algorithm-prefix $ALGORITHM"
