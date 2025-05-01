2559ac5890c4893f5860068afc8894c02e8ac39a
Jan Křůmal <xkruma01@vutbr.cz>
2025-05-01 12:27:29 +0200

    [SCRIPT] Adjustments for possible mx integration

--- JCG/run.sh
+++ JCG/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Tested tool and algorithm
+# Positional parameters
+JCG_PATH=$1
+INPUT_DIR=$2
+MEM_LIMIT="${3:-12G}"
+# note - expecting the validation in the mx tool, not here
+
+# Constants
 TOOL="NativeImage"      
 ALGORITHM="PTA"
-
-# Paths
-JCG_PATH="/mnt/extra/IdeaProjects/BP/JCG"
-INPUT_DIR="input"
 OUTPUT_DIR="testcasesOutput/java/native_image_adapter"
 RESOURCE_DIR="jcg_testcases/src/main/resources"
 PARSER_DIR="jcg_native_image_result_parser"
@@ -20,14 +22,14 @@ mkdir -p "$OUTPUT_DIR"
 # compile tests
 echo "Compiling JCG tests..."
 touch ./$OUTPUT_DIR/$TEST_COMPILE
-sbt -J-Xmx12G "project jcg_testcases" "runMain TestCaseExtractor --rsrcDir ./$RESOURCE_DIR
---outDir ./input --lang java --debug" &> ./$OUTPUT_DIR/$TEST_COMPILE
+sbt -J-Xmx"$MEM_LIMIT" "project jcg_testcases" "runMain TestCaseExtractor --rsrcDir ./$RESOURCE_DIR
+--outDir ./$INPUT_DIR --lang java --debug" &> ./$OUTPUT_DIR/$TEST_COMPILE
 echo "JCG tests compiled, compile output in file $JCG_PATH/$OUTPUT_DIR/$TEST_COMPILE."
 
 # run analysis
 echo "Running analysis with NativeImageJCGAdapter..."
 touch ./$OUTPUT_DIR/$EVALUATION
-sbt -J-Xmx12G "; project jcg_evaluation; runMain FingerprintExtractor -i $JCG_PATH/$INPUT_DIR/java
+sbt -J-Xmx"$MEM_LIMIT" "; project jcg_evaluation; runMain FingerprintExtractor -i $JCG_PATH/$INPUT_DIR/java
  -o $JCG_PATH/$OUTPUT_DIR -l java -d --adapter $TOOL --algorithm-prefix $ALGORITHM" &> ./$OUTPUT_DIR/$EVALUATION
 echo "NativeImageJCGAdapter analysis completed, debug output in file $JCG_PATH/$OUTPUT_DIR/$EVALUATION."
 
